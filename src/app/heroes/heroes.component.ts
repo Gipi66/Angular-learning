417acf4625f4a57d27111554728544f22c68aa1a
Serge <drenozua@gmail.com>
2018-08-10 02:00:30 +0300

    lesson 5 observable resources

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../Hero';
 import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,17 +12,22 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
+  // heroService: HeroService;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() {
+  constructor(private heroService: HeroService) {
+    // this.heroService = heroService;
   }
 
   ngOnInit() {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero) {
